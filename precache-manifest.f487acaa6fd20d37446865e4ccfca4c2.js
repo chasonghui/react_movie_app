3ed4972abd0250ac0573ff2be2a17e84fc08f4b4
chasonghui <chasonghui@naver.com>
2021-02-17 00:38:20 +0900

    Updates

---
+++ precache-manifest.f487acaa6fd20d37446865e4ccfca4c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63d236fe888290135b68104119823eb",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "bde0524648b1df5cc22a",
+    "url": "/react_movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "6280352a5ec74f58c81c",
+    "url": "/react_movie_app/static/js/2.666fd827.chunk.js"
+  },
+  {
+    "revision": "bde0524648b1df5cc22a",
+    "url": "/react_movie_app/static/js/main.4893d25f.chunk.js"
+  },
+  {
+    "revision": "faf18139e1d65c6c220e",
+    "url": "/react_movie_app/static/js/runtime~main.314610f3.js"
+  }
+]);
